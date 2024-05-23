24de763bd47b2849affa23472614e97b512e35e1
[Cloud2] 변우영 <likevangogh24@gmail.com>
2024-05-23 17:30:37 +0900

    Add: BoardEntity에서 시간 코드 삭제 후 BaseTimeEntity 상속

--- src/main/java/com/example/savingsalt/community/board/entity/Board.java
+++ src/main/java/com/example/savingsalt/community/board/entity/Board.java
@@ -1,28 +1,22 @@
 package com.example.savingsalt.community.board.entity;
 
 import com.example.savingsalt.community.category.entity.Category;
-import jakarta.persistence.Column;
+import com.example.savingsalt.global.BaseTimeEntity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Entity
-public class Board {
+public class Board extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,14 +25,6 @@ public class Board {
     private String postTitle;
 
     private String postContent;
-
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime updatedAt;
 
     @ManyToOne
     @JoinColumn(name = "category_id", nullable = false)
