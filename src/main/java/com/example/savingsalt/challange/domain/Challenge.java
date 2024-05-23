560bcd8037e40d2a1e888096982e4b5099d9194d
h3136514 <3136514@naver.com>
2024-05-24 00:00:28 +0900

    #7 Feat: 관계 지연로딩 설정

--- src/main/java/com/example/savingsalt/challange/domain/Challenge.java
+++ src/main/java/com/example/savingsalt/challange/domain/Challenge.java
@@ -3,6 +3,7 @@ package com.example.savingsalt.challange.domain;
 import com.example.savingsalt.global.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -56,7 +57,7 @@ public class Challenge extends BaseTimeEntity {
         HARD
     }
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "badge_id")
     private Badge badge;
 }
