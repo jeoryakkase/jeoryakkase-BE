a49208426c7ef8778bbbd39ae69bf5c672ab5211
h3136514 <3136514@naver.com>
2024-05-24 00:03:35 +0900

    #7 Remove: 불필요한 import문 삭제

--- src/main/java/com/example/savingsalt/challange/domain/Badge.java
+++ src/main/java/com/example/savingsalt/challange/domain/Badge.java
@@ -2,12 +2,9 @@ package com.example.savingsalt.challange.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
