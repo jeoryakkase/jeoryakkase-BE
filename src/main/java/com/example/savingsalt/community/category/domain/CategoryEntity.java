3b3002e171fb3c77749cf62b4d1d42d927f8889d
[Cloud2] 변우영 <likevangogh24@gmail.com>
2024-05-25 20:24:08 +0900

    Rename: 엔터티 이름 뒤에 Entity 추가

--- src/main/java/com/example/savingsalt/community/category/domain/CategoryEntity.java
+++ src/main/java/com/example/savingsalt/community/category/domain/CategoryEntity.java
@@ -5,27 +5,25 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Table(name = "categories")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
-public class Category {
+public class CategoryEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JoinColumn(name = "category_id")
     private Long id;
 
     @Column(nullable = false)
-    @JoinColumn(name = "category_name")
     private String name;
 
     @Column(nullable = false)
-    @JoinColumn(name = "category_content")
     private String content;
 
 
