2412f2e45fc1c3fe62d544d54015123c65fb0c9f
[Cloud2] 변우영 <likevangogh24@gmail.com>
2024-05-25 20:28:18 +0900

    Feat: BoardService를 인터페이스화, 절약팁 게시판 기능 추가

--- src/main/java/com/example/savingsalt/community/board/service/BoardService.java
+++ src/main/java/com/example/savingsalt/community/board/service/BoardService.java
@@ -1,13 +1,19 @@
 package com.example.savingsalt.community.board.service;
 
-import com.example.savingsalt.community.board.repository.BoardRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import com.example.savingsalt.community.board.domain.BoardTypeTipCreateReqDto;
+import com.example.savingsalt.community.board.domain.BoardTypeTipReadResDto;
 
-@RequiredArgsConstructor
-@Service
-public class BoardService {
 
-    private final BoardRepository boardRepository;
+public interface BoardService {
+
+    // 절약팁 게시판 (like, bookmark 기능)
+    BoardTypeTipReadResDto createTipBoard(BoardTypeTipCreateReqDto requestDto);
+
+    // dto 추가 작성 후 작업예정
+//    // 허불허 게시판 (투표게시판)
+//    BoardTypeVoteReadResDto createVoteBoard(BoardTypeVoteCreateReqDto requestDto);
+//    // 소금모아태산 게시판 (챌린지 달성 정보 작성 게시판)
+//    BoardTypeHofReadResDto createHofBoard(BoardTypeHofCreateReqDto requestDto);
+
 
 }
