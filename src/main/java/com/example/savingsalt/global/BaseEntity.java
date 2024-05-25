7adb46777a715f7a6a5c193836236a1dd36ad53d
[Cloud2] 변우영 <likevangogh24@gmail.com>
2024-05-25 20:27:01 +0900

    Rename: BaseEntity로 수정

--- src/main/java/com/example/savingsalt/global/BaseEntity.java
+++ src/main/java/com/example/savingsalt/global/BaseEntity.java
@@ -12,7 +12,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseTimeEntity {
+public abstract class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
