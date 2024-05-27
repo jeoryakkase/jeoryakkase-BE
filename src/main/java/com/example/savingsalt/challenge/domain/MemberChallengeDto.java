747f04bdf1abc7a48d2eb70a82459eff8e4f4757
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-05-27 22:08:15 +0900

    Feat: 회원 챌린지 DTO 필드 구현

--- src/main/java/com/example/savingsalt/challenge/domain/MemberChallengeDto.java
+++ src/main/java/com/example/savingsalt/challenge/domain/MemberChallengeDto.java
@@ -1,5 +1,33 @@
 package com.example.savingsalt.challenge.domain;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class MemberChallengeDto {
-    // TODO MemberChallengeDto 작성
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    private LocalDateTime certifyDate;
+
+    private Boolean challengeProgressStatus;
+
+    private Boolean challengeStatus;
+
+    private Integer challengeConut;
+
+    private Integer challengeTry;
+
+    private String challengeComment;
+
+    private Integer saveMoney;
+
+    private List<CertificationChallengeEntity> certificationChallengeEntities;
+
+    private ChallengeEntity challengeEntity;
 }
