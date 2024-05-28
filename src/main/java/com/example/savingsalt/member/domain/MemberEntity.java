9467e876297ae942028193fc9c541c5de1a685fc
[Cloud2] 변우영 <likevangogh24@gmail.com>
2024-05-28 10:52:36 +0900

    Style: 줄바뀜

--- src/main/java/com/example/savingsalt/member/domain/MemberEntity.java
+++ src/main/java/com/example/savingsalt/member/domain/MemberEntity.java
@@ -1,19 +1,27 @@
 package com.example.savingsalt.member.domain;
 
 import com.example.savingsalt.global.BaseEntity;
+import com.example.savingsalt.member.enums.Role;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Getter
@@ -23,7 +31,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Builder
 @Table(name = "members")
 @EntityListeners(AuditingEntityListener.class)
-public class MemberEntity extends BaseEntity {
+public class MemberEntity extends BaseEntity implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -54,54 +62,54 @@ public class MemberEntity extends BaseEntity {
     @Column(name = "profile_image", nullable = true)
     private String profileImage;
 
-//    @Column(name = "role")
-//    @Enumerated(EnumType.STRING)
-//    private Role role;
-//
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return List.of(new SimpleGrantedAuthority(role.getKey()));
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return email;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return true;
-//    }
-//
-//    public void authorizeUser() {
-//        this.role = Role.MEMBER;
-//    }
-//
-//    public MemberDto toDto() {
-//        return MemberDto.builder()
-//            .id(this.id)
-//            .email(this.email)
-//            .password(this.password)
-//            .nickname(this.nickname)
-//            .age(this.age)
-//            .gender(this.gender)
-//            .income(this.income)
-//            .saving_goal(this.savingGoal)
-//            .build();
-//    }
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.getKey()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void authorizeUser() {
+        this.role = Role.MEMBER;
+    }
+
+    public MemberDto toDto() {
+        return MemberDto.builder()
+            .id(this.id)
+            .email(this.email)
+            .password(this.password)
+            .nickname(this.nickname)
+            .age(this.age)
+            .gender(this.gender)
+            .income(this.income)
+            .saving_goal(this.savingGoal)
+            .build();
+    }
 }
