77a3e23176ac02c925b315317dd43bc8e74e0509
h3136514 <3136514@naver.com>
2024-05-28 21:27:03 +0900

    Feat: 회원별 완료된 챌린지 정보 응답 dto 작성

---
+++ src/main/java/com/example/savingsalt/challenge/domain/MemberChallengeCompleteResDto.java
@@ -0,0 +1,16 @@
+package com.example.savingsalt.challenge.domain;
+
+import java.time.LocalDateTime;
+
+public class MemberChallengeCompleteResDto {
+
+    private String challengeTitle;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    private String challengeTerm;
+
+    private String badgeImage;
+}
