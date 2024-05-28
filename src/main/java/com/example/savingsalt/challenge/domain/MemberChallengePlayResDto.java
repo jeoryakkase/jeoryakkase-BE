83423573ab1a00f5448c368f4a740354af54978e
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-05-28 21:35:20 +0900

    Feat: 참여 중인 챌린지 조회 응답 DTO

--- src/main/java/com/example/savingsalt/challenge/domain/MemberChallengePlayResDto.java
+++ src/main/java/com/example/savingsalt/challenge/domain/MemberChallengePlayResDto.java
@@ -1,11 +1,22 @@
 package com.example.savingsalt.challenge.domain;
 
-import lombok.AllArgsConstructor;
+import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
-@AllArgsConstructor
+@Setter
 public class MemberChallengePlayResDto {
-    // 제목, 챌린지 고정 기간, 인증할 때 업로드한 사진, 인증 여부, 시작 날짜, 종료 날짜(시작 날짜 + 챌린지 고정 기간)
 
+    private String challengeTitle;
+
+    private String certificationImage;
+
+    private Boolean challengeProgressStatus;
+
+    private String challengeTerm;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
 }
