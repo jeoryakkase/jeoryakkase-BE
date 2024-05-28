243f8644c96124f07fd7d9988857ef8a7e504372
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-05-28 21:00:00 +0900

    Feat: 챌린지 생성 요청 DTO

--- src/main/java/com/example/savingsalt/challenge/domain/ChallengeCreateReqDto.java
+++ src/main/java/com/example/savingsalt/challenge/domain/ChallengeCreateReqDto.java
@@ -1,10 +1,13 @@
 package com.example.savingsalt.challenge.domain;
 
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ChallengeCreateReqDto {
 
     private String challengeTitle;
@@ -20,5 +23,7 @@ public class ChallengeCreateReqDto {
     private String challengeTerm;
 
     private ChallengeEntity.ChallengeDifficulty challengeDifficulty;
+
+    private LocalDateTime createAt;
 
 }
