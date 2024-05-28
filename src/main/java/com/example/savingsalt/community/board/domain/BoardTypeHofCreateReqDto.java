b821c4d904bb045803e00f8c4e17d8a0edcf231b
[Cloud2] 변우영 <likevangogh24@gmail.com>
2024-05-28 10:50:09 +0900

    Feat: 챌린지 달성 게시판 Dto 추가

--- src/main/java/com/example/savingsalt/community/board/domain/BoardTypeHofCreateReqDto.java
+++ src/main/java/com/example/savingsalt/community/board/domain/BoardTypeHofCreateReqDto.java
@@ -1,5 +1,15 @@
 package com.example.savingsalt.community.board.domain;
 
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class BoardTypeHofCreateReqDto {
+
+    private String title;
+    private String contents;
+    private List<Long> challengeAchievementsIds; // // 사용자가 완료하고 자랑하기를 선택한 챌린지 ID들
 
 }
