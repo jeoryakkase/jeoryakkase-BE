9f4dce8fb284569ea681e9919c2af0396dbc9c0f
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-05-28 21:43:02 +0900

    Refactor: DTO 관련 어노테이션 수정

--- src/main/java/com/example/savingsalt/challenge/domain/ChallengeCreateReqDto.java
+++ src/main/java/com/example/savingsalt/challenge/domain/ChallengeCreateReqDto.java
@@ -1,13 +1,17 @@
 package com.example.savingsalt.challenge.domain;
 
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Setter
-@NoArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class ChallengeCreateReqDto {
 
     private String challengeTitle;
--- src/main/java/com/example/savingsalt/challenge/domain/MemberChallengePlayResDto.java
+++ src/main/java/com/example/savingsalt/challenge/domain/MemberChallengePlayResDto.java
@@ -1,11 +1,16 @@
 package com.example.savingsalt.challenge.domain;
 
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class MemberChallengePlayResDto {
 
     private String challengeTitle;
