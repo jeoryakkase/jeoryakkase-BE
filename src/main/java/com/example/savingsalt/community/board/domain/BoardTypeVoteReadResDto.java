59263f4fd00eb852d9342d0308f42ed084035e73
[Cloud2] 변우영 <likevangogh24@gmail.com>
2024-05-28 20:00:58 +0900

    Feat: 투표 게시판, 팁 게시판 Dto에 작성일자 추가

--- src/main/java/com/example/savingsalt/community/board/domain/BoardTypeVoteReadResDto.java
+++ src/main/java/com/example/savingsalt/community/board/domain/BoardTypeVoteReadResDto.java
@@ -1,12 +1,14 @@
 package com.example.savingsalt.community.board.domain;
 
 import com.example.savingsalt.community.poll.domain.PollResDto;
+import com.example.savingsalt.global.BaseEntity;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class BoardTypeVoteReadResDto {
+public class BoardTypeVoteReadResDto extends BaseEntity {
 
     private Long id;
     private String nickname;
@@ -14,5 +16,13 @@ public class BoardTypeVoteReadResDto {
     private String contents;
     private int boardHits;
     private PollResDto pollResData;
+
+    public LocalDateTime getCreatedAt() {
+        return this.getCreatedAt();
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return this.getModifiedAt();
+    }
 
 }
