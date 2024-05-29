208b3d74ba114cb9cd7b748d3c63ec5ac94b9b3b
[Cloud2 박수빈] <subin6985@gmail.com>
2024-05-30 02:55:52 +0900

    Fix: 토큰 기반으로 로그인, 로그아웃 구현

--- src/main/java/com/example/savingsalt/member/service/LoginService.java
+++ src/main/java/com/example/savingsalt/member/service/LoginService.java
@@ -3,25 +3,38 @@ package com.example.savingsalt.member.service;
 import com.example.savingsalt.member.domain.MemberEntity;
 import com.example.savingsalt.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class LoginService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
-    private static final Logger logger = LoggerFactory.getLogger(LoginService.class);
-
     @Override
-    public MemberEntity loadUserByUsername(String email) {
+    public UserDetails loadUserByUsername(String email) {
         MemberEntity memberEntity = memberRepository.findByEmail(email)
             .orElseThrow(
                 () -> new IllegalArgumentException(("User not found with email: " + email)));
-        logger.info("User found with email: {}", email);
+        log.info("User found with email: {}", email);
         return memberEntity;
+    }
+
+    private UserDetails createUserDetails(MemberEntity memberEntity) {
+        return MemberEntity.builder()
+            .email(memberEntity.getEmail())
+            .password(memberEntity.getPassword())
+            .nickname(memberEntity.getNickname())
+            .age(memberEntity.getAge())
+            .gender(memberEntity.getGender())
+            .income(memberEntity.getIncome())
+            .savingGoal(memberEntity.getSavingGoal())
+            .role(memberEntity.getRole())
+            .profileImage(memberEntity.getProfileImage())
+            .build();
     }
 }
---
+++ src/main/java/com/example/savingsalt/member/service/RefreshTokenService.java
@@ -0,0 +1,18 @@
+package com.example.savingsalt.member.service;
+
+import com.example.savingsalt.member.domain.RefreshToken;
+import com.example.savingsalt.member.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+            .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+}
