e4db67f622560c9e3c56113e907c41d9fb255375
h3136514 <3136514@naver.com>
2024-05-30 22:00:10 +0900

    Feat: 챌린지 테이블 Mapper 인터페이스 추가

--- src/main/java/com/example/savingsalt/challenge/mapper/ChallengeMainMapper.java
+++ src/main/java/com/example/savingsalt/challenge/mapper/ChallengeMainMapper.java
@@ -1,12 +1,24 @@
 package com.example.savingsalt.challenge.mapper;
 
+import com.example.savingsalt.challenge.domain.dto.ChallengeCreateReqDto;
+import com.example.savingsalt.challenge.domain.dto.ChallengeDto;
+import com.example.savingsalt.challenge.domain.dto.ChallengeReadResDto;
+import com.example.savingsalt.challenge.domain.dto.ChallengeUpdateReqDto;
 import com.example.savingsalt.challenge.domain.dto.MemberChallengeDto;
+import com.example.savingsalt.challenge.domain.entity.ChallengeEntity;
 import com.example.savingsalt.challenge.domain.entity.MemberChallengeEntity;
 import com.example.savingsalt.global.EntityMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.data.domain.Page;
 
 public interface ChallengeMainMapper {
+
+    @Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
+    interface ChallengeMapper extends
+        EntityMapper<ChallengeEntity, ChallengeDto> {
+
+    }
 
     @Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
     interface MemberChallengeMapper extends
