f5a29df7259d0d42db787bbfb75ff3722336c382
[Cloud2 박수빈] <subin6985@gmail.com>
2024-06-02 02:31:15 +0900

    Fix: 순환 참조 해결

--- src/main/java/com/example/savingsalt/member/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/savingsalt/member/repository/RefreshTokenRepository.java
@@ -10,6 +10,4 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
     Optional<RefreshToken> findByMemberId(Long memberId);
 
     Optional<RefreshToken> findByRefreshToken(String refreshToken);
-
-    void deleteByMember(MemberEntity member);
 }
