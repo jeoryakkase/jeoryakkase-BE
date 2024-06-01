7a2bc793dbbbab9844c765f74781316f5206c442
h3136514 <3136514@naver.com>
2024-06-01 17:58:46 +0900

    Feat: Badge 도메인 컨트롤러 작성

--- src/main/java/com/example/savingsalt/badge/controller/BadgeController.java
+++ src/main/java/com/example/savingsalt/badge/controller/BadgeController.java
@@ -10,7 +10,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,25 +39,25 @@ public class BadgeController {
     public ResponseEntity<List<BadgeDto>> getAllBadges() {
         List<BadgeDto> badges = badgeService.getAllBadges();
 
-        if (badges != null) {
+        if (!ObjectUtils.isEmpty(badges)) {
             return ResponseEntity.ok(badges);
         } else {
-            return ResponseEntity.noContent().build();
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
     }
 
     // 회원 목표 달성 뱃지 정보 조회
     @Operation(summary = "Get MemberGoalBadges", description = "Gets all existing memberGoalbadge information")
-    @GetMapping("/members/{memberId}/goal/badges")
+    @GetMapping("/members/{memberId}/goals/badges")
     public ResponseEntity<List<MemberGoalBadgeResDto>> getMemberGoalBadges(
         @Parameter(description = "ID of the member") @PathVariable Long memberId) {
         List<MemberGoalBadgeResDto> memberGoalBadgesResDto = badgeService.getMemberGoalBadges(
             memberId);
 
-        if (memberGoalBadgesResDto != null) {
+        if (!ObjectUtils.isEmpty(memberGoalBadgesResDto)) {
             return ResponseEntity.ok(memberGoalBadgesResDto);
         } else {
-            return ResponseEntity.noContent().build();
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
     }
 
@@ -66,10 +69,10 @@ public class BadgeController {
         List<MemberChallengeBadgeResDto> memberChallengeBadgeResDto = badgeService.getMemberChallengeBadges(
             memberId);
 
-        if (memberChallengeBadgeResDto != null) {
+        if (!ObjectUtils.isEmpty(memberChallengeBadgeResDto)) {
             return ResponseEntity.ok(memberChallengeBadgeResDto);
         } else {
-            return ResponseEntity.noContent().build();
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
     }
 
@@ -81,9 +84,50 @@ public class BadgeController {
         BadgeDto createdBadgeDto = badgeService.createBadge(badgeCreateReqDto);
 
         if (createdBadgeDto != null) {
-            return ResponseEntity.ok(createdBadgeDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdBadgeDto);
         } else {
-            return ResponseEntity.noContent().build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
+    }
+
+    // 회원 목표 달성 뱃지 생성
+    @Operation(summary = "Create memberGoalbadge", description = "Create a memberGoalbadge")
+    @PostMapping("/members/{memberId}/goals/badges{badgeId}")
+    public ResponseEntity<BadgeDto> createMemberGoalBadge(
+        @Parameter(description = "ID of the badge") @PathVariable Long badgeId,
+        @Parameter(description = "ID of the member") @PathVariable Long memberId) {
+        BadgeDto createdMemberGoalBadgeDto = badgeService.createMemberGoalBadge(badgeId, memberId);
+
+        if (createdMemberGoalBadgeDto != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdMemberGoalBadgeDto);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    // 뱃지 정보 수정
+    @Operation(summary = "Update badge", description = "Update a badge")
+    @PutMapping("/badges/{badgeId}")
+    public ResponseEntity<BadgeDto> updateBadge(
+        @Parameter(description = "ID of the badge") @PathVariable Long badgeId,
+        @Parameter(description = "Information about the badge to be updated") @RequestBody BadgeUpdateReqDto badgeUpdateReqDto) {
+        BadgeDto updatedBadgeDto = badgeService.updateBadge(badgeId, badgeUpdateReqDto);
+
+        if (updatedBadgeDto != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(updatedBadgeDto);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    // 뱃지 삭제
+    @Operation(summary = "Delete badge", description = "Delete a badge")
+    @DeleteMapping("/badges/{badgeId}")
+    public ResponseEntity<Void> deleteBadge(
+        @Parameter(description = "ID of the badge") @PathVariable Long badgeId) {
+
+        badgeService.deleteBadge(badgeId);
+
+        return ResponseEntity.ok().build();
     }
 }
