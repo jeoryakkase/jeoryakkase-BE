d9b41d32212273da9a480c4c9d05ccb7e093f97b
HoJun Lee <lhjunit5138@naver.com>
2024-06-03 12:35:18 +0900

    Feat: GoalController 클래스에 @Valid 추가

--- src/main/java/com/example/savingsalt/goal/controller/GoalController.java
+++ src/main/java/com/example/savingsalt/goal/controller/GoalController.java
@@ -5,6 +5,7 @@ import com.example.savingsalt.goal.domain.dto.GoalResponseDto;
 import com.example.savingsalt.goal.domain.dto.GoalUpdateReqDto;
 import com.example.savingsalt.goal.domain.entity.GoalEntity;
 import com.example.savingsalt.goal.service.GoalService;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,30 +25,28 @@ public class GoalController {
 
     private final GoalService goalService;
 
+    // TODO: 나중에 JWT 토큰과 관련하여 사용자의 정보 처리 로직도 추가 요망
     @PostMapping("/goals")
     public ResponseEntity<GoalResponseDto> createGoal(
-        @RequestBody GoalCreateReqDto goalCreateReqDto) {
+        @Valid @RequestBody GoalCreateReqDto goalCreateReqDto) {
         GoalResponseDto created = goalService.createGoal(goalCreateReqDto);
-        return (created != null) ?
-            ResponseEntity.status(HttpStatus.CREATED).body(created) :
-            ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        return (created != null) ? ResponseEntity.status(HttpStatus.CREATED).body(created)
+            : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     @GetMapping("/goals")
     public ResponseEntity<List<GoalResponseDto>> getAllGoals() {
         List<GoalResponseDto> goals = goalService.getAllGoals();
-        return (goals != null && !goals.isEmpty()) ?
-            ResponseEntity.ok(goals) :
-            ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return (goals != null && !goals.isEmpty()) ? ResponseEntity.ok(goals)
+            : ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @PutMapping("/goals/{id}")
     public ResponseEntity<GoalResponseDto> updateGoal(
         @PathVariable Long id,
-        @RequestBody GoalUpdateReqDto goalUpdateReqDto) {
+        @Valid @RequestBody GoalUpdateReqDto goalUpdateReqDto) {
         GoalResponseDto updated = goalService.updateGoal(id, goalUpdateReqDto);
-        return (updated != null) ?
-            ResponseEntity.ok(updated) :
-            ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return (updated != null) ? ResponseEntity.ok(updated)
+            : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
