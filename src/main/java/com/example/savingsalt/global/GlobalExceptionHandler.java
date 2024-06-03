aecb7d5af397cc98ffd71b9fc6ac9463ffde875f
[Cloud2] 변우영 <likevangogh24@gmail.com>
2024-06-03 18:53:36 +0900

    Feat: 게시판, 댓글 예외처리 추가

--- src/main/java/com/example/savingsalt/global/GlobalExceptionHandler.java
+++ src/main/java/com/example/savingsalt/global/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.savingsalt.global;
 
+import com.example.savingsalt.community.board.exception.BoardException;
+import com.example.savingsalt.community.board.exception.CommentException;
 import com.example.savingsalt.global.ChallengeException.BadgeNotFoundException;
 import com.example.savingsalt.global.ChallengeException.ChallengeNotFoundException;
 import com.example.savingsalt.member.exception.MemberException;
@@ -54,5 +56,52 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ChallengeNotFoundException.class)
     public ResponseEntity<String> handleChallengeNotFoundException(ChallengeNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    // 게시판 관련 예외처리
+    @ExceptionHandler(BoardException.UnauthorizedCreateException.class)
+    public ResponseEntity<String> handleUnauthorizedCreateException(
+        BoardException.UnauthorizedCreateException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+    @ExceptionHandler(BoardException.UnauthorizedUpdateException.class)
+    public ResponseEntity<String> handleUnauthorizedUpdateException(
+        BoardException.UnauthorizedUpdateException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+    @ExceptionHandler(BoardException.UnauthorizedDeleteException.class)
+    public ResponseEntity<String> handleUnauthorizedDeleteException(
+        BoardException.UnauthorizedDeleteException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(BoardException.EmptyBoardException.class)
+    public ResponseEntity<String> handleEmptyBoardException(
+        BoardException.EmptyBoardException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(BoardException.InternalServerErrorException.class)
+    public ResponseEntity<String> handleInternalServerErrorException(
+        BoardException.InternalServerErrorException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // 댓글 관련 예외 처리
+    @ExceptionHandler(CommentException.CommentNotFoundException.class)
+    public ResponseEntity<String> handleCommentNotFoundException(
+        CommentException.CommentNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(CommentException.ValidateAuthorForUpdate.class)
+    public ResponseEntity<String> handleValidateAuthorForUpdate(
+        CommentException.ValidateAuthorForUpdate ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(CommentException.ValidateAuthorForDelete.class)
+    public ResponseEntity<String> handleValidateAuthorForDelete(
+        CommentException.ValidateAuthorForDelete ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 }
