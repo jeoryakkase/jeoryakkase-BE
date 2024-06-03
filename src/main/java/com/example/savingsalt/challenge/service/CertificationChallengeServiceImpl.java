4cadbe90c58fd923f9951d03cf197e1415b3ee69
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-06-03 15:55:18 +0900

    Remove: 불필요한 생성자 주입 제거

--- src/main/java/com/example/savingsalt/challenge/service/CertificationChallengeServiceImpl.java
+++ src/main/java/com/example/savingsalt/challenge/service/CertificationChallengeServiceImpl.java
@@ -5,11 +5,8 @@ import com.example.savingsalt.challenge.domain.entity.CertificationChallengeEnti
 import com.example.savingsalt.challenge.domain.entity.MemberChallengeEntity;
 import com.example.savingsalt.challenge.mapper.ChallengeMainMapper.CertificationChallengeMapper;
 import com.example.savingsalt.challenge.repository.CertificationChallengeRepository;
-import com.example.savingsalt.challenge.repository.MemberChallengeRepository;
-import com.example.savingsalt.global.ChallengeException.MemberChallengeNotFoundException;
 import jakarta.transaction.Transactional;
 import java.time.LocalDateTime;
-import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,39 +15,29 @@ public class CertificationChallengeServiceImpl {
 
     private final CertificationChallengeRepository certificationChallengeRepository;
     private final CertificationChallengeMapper certificationChallengeMapper;
-    private final MemberChallengeRepository memberChallengeRepository;
 
     public CertificationChallengeServiceImpl(
         CertificationChallengeRepository certificationChallengeRepository
-        , CertificationChallengeMapper certificationChallengeMapper
-        , MemberChallengeRepository memberChallengeRepository) {
+        , CertificationChallengeMapper certificationChallengeMapper) {
 
         this.certificationChallengeRepository = certificationChallengeRepository;
         this.certificationChallengeMapper = certificationChallengeMapper;
-        this.memberChallengeRepository = memberChallengeRepository;
     }
 
     // 회원 챌린지 일일 인증 생성
-    public void createCertificationChallenge(Long memberChallngeId,
+    public void createCertificationChallenge(MemberChallengeEntity memberChallengeEntity,
         CertificationChallengeReqDto certificationChallengeReqDto) {
 
-        Optional<MemberChallengeEntity> memberChallengeEntityOpt = memberChallengeRepository.findById(
-            memberChallngeId);
-
-        if (memberChallengeEntityOpt.isPresent()) {
             LocalDateTime currentDateTime = LocalDateTime.now();
 
             CertificationChallengeEntity certificationChallengeEntity = certificationChallengeMapper.toEntity(
                 certificationChallengeReqDto);
 
             certificationChallengeEntity.toBuilder()
-                .memberChallengeEntity(memberChallengeEntityOpt.get())
+                .memberChallengeEntity(memberChallengeEntity)
                 .certificationDate(currentDateTime);
 
             certificationChallengeRepository.save(certificationChallengeEntity);
-        } else {
-            throw new MemberChallengeNotFoundException();
-        }
     }
 
 }
