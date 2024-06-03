a9d9821f42057f0ee8339c702be9629e5acddca2
HoJun Lee <lhjunit5138@naver.com>
2024-06-03 14:54:20 +0900

    Feat: GoalController와 GoalCreateReqDto에 Swagger 적용

--- src/main/java/com/example/savingsalt/goal/controller/GoalController.java
+++ src/main/java/com/example/savingsalt/goal/controller/GoalController.java
@@ -5,6 +5,9 @@ import com.example.savingsalt.goal.domain.dto.GoalResponseDto;
 import com.example.savingsalt.goal.domain.dto.GoalUpdateReqDto;
 import com.example.savingsalt.goal.domain.entity.GoalEntity;
 import com.example.savingsalt.goal.service.GoalService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -26,14 +29,17 @@ public class GoalController {
     private final GoalService goalService;
 
     // TODO: 나중에 JWT 토큰과 관련하여 사용자의 정보 처리 로직도 추가 요망
+    @Operation(summary = "새로운 목표 생성", description = "제공된 데이터를 기반으로 새로운 목표를 생성합니다.")
     @PostMapping("/goals")
     public ResponseEntity<GoalResponseDto> createGoal(
-        @Valid @RequestBody GoalCreateReqDto goalCreateReqDto) {
+        @Valid @Parameter(description = "목표 생성을 위한 요청 데이터", required = true, schema = @Schema(implementation = GoalCreateReqDto.class))
+        @RequestBody GoalCreateReqDto goalCreateReqDto) {
         GoalResponseDto created = goalService.createGoal(goalCreateReqDto);
         return (created != null) ? ResponseEntity.status(HttpStatus.CREATED).body(created)
             : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
+    @Operation(summary = "모든 목표 조회", description = "모든 목표를 검색하여 가져옵니다.")
     @GetMapping("/goals")
     public ResponseEntity<List<GoalResponseDto>> getAllGoals() {
         List<GoalResponseDto> goals = goalService.getAllGoals();
@@ -41,10 +47,13 @@ public class GoalController {
             : ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @Operation(summary = "목표 수정", description = "제공된 데이터를 기반으로 기존 목표를 업데이트합니다.")
     @PutMapping("/goals/{id}")
     public ResponseEntity<GoalResponseDto> updateGoal(
+        @Parameter(description = "수정할 목표의 고유 ID", required = true, example = "1")
         @PathVariable Long id,
-        @Valid @RequestBody GoalUpdateReqDto goalUpdateReqDto) {
+        @Valid @Parameter(description = "목표 수정을 위한 요청 데이터", required = true, schema = @Schema(implementation = GoalUpdateReqDto.class))
+        @RequestBody GoalUpdateReqDto goalUpdateReqDto) {
         GoalResponseDto updated = goalService.updateGoal(id, goalUpdateReqDto);
         return (updated != null) ? ResponseEntity.ok(updated)
             : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
--- src/main/java/com/example/savingsalt/goal/domain/dto/GoalCreateReqDto.java
+++ src/main/java/com/example/savingsalt/goal/domain/dto/GoalCreateReqDto.java
@@ -1,6 +1,7 @@
 package com.example.savingsalt.goal.domain.dto;
 
 import com.example.savingsalt.goal.domain.entity.GoalEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -21,17 +22,22 @@ import lombok.ToString;
 public class GoalCreateReqDto {
 
     @NotEmpty(message = "목표 제목은 필수입력 사항입니다.")
+    @Schema(description = "목표 제목", example = "100만원 모으기", required = true)
     private String goalTitle;   // 목표 제목
 
     @NotNull(message = "목표 금액은 필수입력 사항입니다.")
     @Positive(message = "목표 금액은 양수만 입력할 수 있습니다.")
+    @Schema(description = "목표 금액", example = "1000000", required = true)
     private Long goalAmount;    // 목표 금액
 
+    @Schema(description = "목표 사진 URL", example = "/devs/images/hello.jpg")
     private String goalImage;   // 목표 이미지
 
+    @Schema(description = "목표 시작 일정", example = "2024-06-03T04:37:53.465Z", required = true)
     @NotNull(message = "목표 시작 일정은 필수입니다.")
     private LocalDateTime goalStartDate;    // 목표 시작 일정
 
+    @Schema(description = "목표 끝나는 일정", example = "2024-07-20T04:37:53.465Z", required = true)
     @NotNull(message = "목표 끝나는 일정은 필수입니다.")
     private LocalDateTime goalEndDate;      // 목표 끝나는 일정
 
