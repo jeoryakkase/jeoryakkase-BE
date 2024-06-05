6ead2e695662b3589c9cb61b1da04f45e70a41e5
h3136514 <3136514@naver.com>
2024-06-05 14:23:45 +0900

    Fix: Badge 생성 검증 어노테이션 변경

--- src/main/java/com/example/savingsalt/badge/domain/dto/BadgeCreateReqDto.java
+++ src/main/java/com/example/savingsalt/badge/domain/dto/BadgeCreateReqDto.java
@@ -23,7 +23,7 @@ public class BadgeCreateReqDto {
     private String badgeDesc;
 
     @NotBlank(message = "뱃지 사진은 필수입력 사항입니다.")
-    @Max(value = 3000, message = "이미지 URL은 최대 3000자까지 저장 가능합니다.")
+    @Size(max = 3000, message = "이미지 URL은 최대 3000자까지 저장 가능합니다.")
     private String badgeImage;
 
     @NotBlank(message = "뱃지 종류는 필수입력 사항입니다.")
