954b77623029086985e0794c4df0e79572f56978
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-06-06 10:35:11 +0900

    Feat: 회원 챌린지 성공 조건 실패시 예외 처리

--- src/main/java/com/example/savingsalt/challenge/exception/ChallengeException.java
+++ src/main/java/com/example/savingsalt/challenge/exception/ChallengeException.java
@@ -15,4 +15,11 @@ public class ChallengeException {
             super("해당 챌린지를 찾을 수 없습니다.");
         }
     }
+
+    public static class MemberChallengeFailureException extends RuntimeException {
+
+        public MemberChallengeFailureException() {
+            super("회원 챌린지 조건에 만족하지 않아 챌린지 인증이 실패했습니다.");
+        }
+    }
 }
