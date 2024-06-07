68112f42ee239b537453c147919570e94fa0161c
HoJun Lee <lhjunit5138@naver.com>
2024-06-07 16:29:15 +0900

    Feat: GoalCategory 생성, 조회 구현

---
+++ src/main/java/com/example/savingsalt/goal/service/GoalCategoryService.java
@@ -0,0 +1,52 @@
+package com.example.savingsalt.goal.service;
+
+import com.example.savingsalt.goal.domain.dto.GoalCategoryCreateReqDto;
+import com.example.savingsalt.goal.domain.dto.GoalCategoryResDto;
+import com.example.savingsalt.goal.domain.entity.GoalCategoryEntity;
+import com.example.savingsalt.goal.repository.GoalCategoryRepository;
+import com.example.savingsalt.member.domain.MemberEntity;
+import com.example.savingsalt.member.exception.MemberException.MemberNotFoundException;
+import com.example.savingsalt.member.repository.MemberRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class GoalCategoryService {
+
+    private final GoalCategoryRepository goalCategoryRepository;
+    private final MemberRepository memberRepository;
+
+    public void createGoalCategory(GoalCategoryCreateReqDto goalCategoryCreateReqDto,
+        String email) {
+        // email 을 이용해 MemberEntity 조회
+        MemberEntity memberEntity = memberRepository.findByEmail(email)
+            .orElseThrow(() -> new MemberNotFoundException("email", email));
+
+        // GoalCategory 엔티티로 변환
+        GoalCategoryEntity goalCategory = GoalCategoryEntity.builder()
+            .certificationDetails(goalCategoryCreateReqDto.getCertificationDetails())
+            .memberEntity(memberEntity)
+            .build();
+
+        // 데이터베이스에 저장
+        goalCategoryRepository.save(goalCategory);
+    }
+
+    public List<GoalCategoryResDto> getGoalCategoriesByUser(String email) {
+        // email 을 이용해 MemberEntity 조회
+        MemberEntity memberEntity = memberRepository.findByEmail(email)
+            .orElseThrow(() -> new MemberNotFoundException("email", email));
+
+        // 사용자가 작성한 GoalCategory 조회
+        List<GoalCategoryEntity> goalCategories = goalCategoryRepository.findByMemberEntity(
+            memberEntity);
+
+        return goalCategories.stream()
+            .map(GoalCategoryResDto::fromEntity)
+            .collect(Collectors.toList());
+    }
+}
+
