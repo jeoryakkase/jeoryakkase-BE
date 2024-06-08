c2c4c8ae3f8032c5bab2ed7e5188b91b52ba4edf
tunacan0329 <tunacan0329@gmail.com>
2024-06-08 15:21:26 +0900

    Fix: 북마크 api와 메서드 수정, 예외처리 추가

--- src/main/java/com/example/savingsalt/global/GlobalExceptionHandler.java
+++ src/main/java/com/example/savingsalt/global/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import com.example.savingsalt.challenge.exception.ChallengeException.InvalidChal
 import com.example.savingsalt.challenge.exception.ChallengeException.MemberChallengeFailureException;
 import com.example.savingsalt.community.board.exception.BoardException;
 import com.example.savingsalt.community.board.exception.BoardException.BoardNotFoundException;
+import com.example.savingsalt.community.bookmark.exception.BookmarkException;
 import com.example.savingsalt.community.comment.exception.CommentException;
 import com.example.savingsalt.community.like.exception.LikeException;
 import com.example.savingsalt.community.poll.exception.PollException;
@@ -201,6 +202,17 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(LikeException.BoardNotFoundException.class)
     public ResponseEntity<String> handleBoardNotFoundException(LikeException.BoardNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    // 북마크 예외 처리
+    @ExceptionHandler(BookmarkException.MemberNotFoundException.class)
+    public ResponseEntity<String> handleMemberNotFoundException(BookmarkException.MemberNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BookmarkException.BoardNotFoundException.class)
+    public ResponseEntity<String> handleBoardNotFoundException(BookmarkException.BoardNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
