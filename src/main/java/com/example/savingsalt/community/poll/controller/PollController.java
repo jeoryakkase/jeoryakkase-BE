978afdf080bd4936feea8b7a8e4ed69f08e7400c
tunacan0329 <tunacan0329@gmail.com>
2024-06-09 23:02:46 +0900

    Feat: 투표 예외처리 추가

--- src/main/java/com/example/savingsalt/community/poll/controller/PollController.java
+++ src/main/java/com/example/savingsalt/community/poll/controller/PollController.java
@@ -3,6 +3,9 @@ package com.example.savingsalt.community.poll.controller;
 import com.example.savingsalt.community.poll.domain.PollResultDto;
 import com.example.savingsalt.community.poll.domain.PollVoteReqDto;
 import com.example.savingsalt.community.poll.service.PollService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,12 +20,23 @@ public class PollController {
     @Autowired
     private PollService pollService;
 
+    @Operation(summary = "투표하기", description = "특정 투표에 대해 회원이 투표합니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "투표 성공"),
+        @ApiResponse(responseCode = "404", description = "투표 또는 회원을 찾을 수 없음"),
+        @ApiResponse(responseCode = "409", description = "사용자가 이미 투표함")
+    })
     @PostMapping("/{pollId}/vote")
     public ResponseEntity<Void> vote(@PathVariable Long pollId, @RequestBody PollVoteReqDto pollVoteReqDto) {
         pollService.vote(pollId, pollVoteReqDto.getMemberId(), pollVoteReqDto.getPollVoteChoice());
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(summary = "투표 결과 조회", description = "특정 투표의 결과를 조회합니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "조회 성공"),
+        @ApiResponse(responseCode = "404", description = "투표를 찾을 수 없음")
+    })
     @GetMapping("/{pollId}/results")
     public ResponseEntity<PollResultDto> getPollResults(@PathVariable Long pollId) {
         PollResultDto pollResultDto = pollService.getPollResults(pollId);
