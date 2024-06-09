ffb7f11ec9410c67c41a7dc53cf18017c82f7fd5
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-06-09 14:15:25 +0900

    Feat: 챌린지 예외 처리 메시지 enum 클래스 생성

---
+++ src/main/java/com/example/savingsalt/challenge/exception/ChallengeExceptionType.java
@@ -0,0 +1,16 @@
+package com.example.savingsalt.challenge.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ChallengeExceptionType {
+    NOT_FOUND_CHALLENGE("챌린지가 존재하지 않습니다."),
+    NOT_FOUND_MEMBER_CHALLENGE("회원 챌린지가 존재하지 않습니다."),
+    NOT_FOUND_CERTIFICATION_CHALLENGE("챌린지 인증 테이블이 존재하지 않습니다."),
+    INVALID_CHALLENGE_TYPE("챌린지 목표 금액, 챌린지 목표 횟수 중 하나만 꼭 존재해야 됩니다."),
+    ALREADY_SUCCEEDED("이미 완료된 챌린지 입니다.");
+
+    private final String exceptionMessage;
+}
