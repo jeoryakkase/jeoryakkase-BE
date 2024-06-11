b8c60da0056e393ac35b518b4885cc2a62c8a734
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-06-11 14:27:13 +0900

    Refactor: AWS S3 업로드 S3Service.uploads()로 코드 간소화

--- src/main/java/com/example/savingsalt/config/s3/S3Service.java
+++ src/main/java/com/example/savingsalt/config/s3/S3Service.java
@@ -4,6 +4,8 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -37,5 +39,40 @@ public class S3Service {
         return String.format(
             "https://s3.ap-southeast-2.amazonaws.com/my.eliceproject.s3.bucket/"
                 + uploadFileName);
+    }
+
+    // 다중 파일 업로드
+    public List<String> uploads(List<MultipartFile> multipartFiles) throws IOException {
+        List<String> imageUrls = new ArrayList<>();
+        String timestamp = String.valueOf(System.currentTimeMillis());
+
+        for (MultipartFile file : multipartFiles) {
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(file.getContentType());
+            objectMetadata.setContentLength(file.getSize());
+
+            PutObjectRequest putObjectRequest;
+
+            String uploadFileName = file.getOriginalFilename() + "/" + timestamp;
+            // test01.jpg/0043885293124
+            // https://s3.ap-southeast-2.amazonaws.com/my.eliceproject.s3.bucket/test01.jpg/0043885293124
+
+            putObjectRequest = new PutObjectRequest(
+                "my.eliceproject.s3.bucket",
+                uploadFileName,
+                file.getInputStream(),
+                objectMetadata
+            );
+
+            amazonS3Client.putObject(putObjectRequest);
+
+            String imageUrl = String.format(
+                "https://s3.ap-southeast-2.amazonaws.com/my.eliceproject.s3.bucket/"
+                    + uploadFileName);
+
+            imageUrls.add(imageUrl);
+        }
+
+        return imageUrls;
     }
 }
