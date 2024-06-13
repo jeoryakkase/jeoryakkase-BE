7f7707b7c1f2bb5f01f9132f2f98335300d205ac
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-06-13 15:25:25 +0900

    Fix: 회원 챌린지 참여 인원 JPA 메서드 에러 해결

--- src/main/java/com/example/savingsalt/challenge/repository/MemberChallengeRepository.java
+++ src/main/java/com/example/savingsalt/challenge/repository/MemberChallengeRepository.java
@@ -2,11 +2,11 @@ package com.example.savingsalt.challenge.repository;
 
 import com.example.savingsalt.challenge.domain.entity.ChallengeEntity;
 import com.example.savingsalt.challenge.domain.entity.MemberChallengeEntity;
-import com.example.savingsalt.challenge.domain.entity.MemberChallengeEntity.ChallengeStatus;
 import com.example.savingsalt.member.domain.entity.MemberEntity;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,6 +16,9 @@ public interface MemberChallengeRepository extends JpaRepository<MemberChallenge
 
     List<MemberChallengeEntity> findAllByChallengeEntity(ChallengeEntity challengeEntity);
 
-    @Query("SELECT COUNT(c) FROM MemberChallengeEntity c WHERE c.challengeStatus = :challengeStatus AND c.challengeEntity = : challengeEntity")
-    long countByChallengeStatusAndChallengeEntity(ChallengeStatus challengeStatus, ChallengeEntity challengeEntity);
+    @Query("SELECT COUNT(c) FROM MemberChallengeEntity c WHERE c.challengeStatus = :status AND c.challengeEntity = :entity")
+    long countByChallengeStatusAndChallengeEntity(
+        @Param("status") MemberChallengeEntity.ChallengeStatus challengeStatus,
+        @Param("entity") ChallengeEntity challengeEntity);
+
 }
